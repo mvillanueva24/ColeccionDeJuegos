91468972a0edaf68379b53feb2fa282b2405d2a6
mvillanueva24 <mariorvillanueva24@gmail.com>
2022-05-12 23:34:59 -0700

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Mario Villanueva Linares on 5/12/22.
+//  Copyright © 2022 mvillanueva24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
